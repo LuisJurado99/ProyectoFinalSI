94bc8e3b32be96c8da54377d78e24041db77acaf
cfonseca2 <cfonseca2@outlook.com>
2020-06-10 19:13:20 -0500

    Se crea el modelo de Perfume

--- models/Perfume.js
+++ models/Perfume.js
@@ -1,1 +1,13 @@
-dj nsfjvwrohvnworjvn
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PerfumeSchema = Schema({
+  marca: String,
+  aroma: String,
+  cantidad: Number,
+  precio: Number,
+  tipo: String,
+  foto: String
+});
+
+module.exports = mongoose.model('Perfume', PerfumeSchema);
